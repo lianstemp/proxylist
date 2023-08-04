d7271d45f4e1aca9e36c530ffbbd783358497fc5
MRXAZK <mrxazk@protonmail.com>
2023-08-04 16:44:59 +0700

    Update API

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import json
 
 app = FastAPI()
 
-@app.get("/proxy/")
+@app.get("/")
 async def get_proxy_list():
     with open("proxy-list/data.json") as f:
         data = json.load(f)
     return data
 
 @app.get("/proxy-txt/")
-async def get_proxy_list():
-    with open("proxy-list/data.txt") as f:
-        data = json.load(f)
-    return data
+async def get_proxy_list_txt():
+    return FileResponse("proxy-list/data.txt", filename="data.txt")
 
 @app.get("/proxy-geo/")
-async def get_proxy_list():
+async def get_proxy_list_geo():
     with open("proxy-list/data-with-geolocation.json") as f:
         data = json.load(f)
     return data
